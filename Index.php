3d4030e89d80166a4a049d0d04465adb2d9d0c0e
catmaldo03 <138506870+catmaldo03@users.noreply.github.com>
2023-11-06 15:08:08 -0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,12 +1,149 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Document</title>
 </head>
+
 <body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>   
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg bg-light px-3 py-1">
+                    <div class="container-fluid">
+                        <div>
+                            <img src="imagen/logo.png" alt="catg-logo" class="nav-logo">
+                        </div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Nosotros</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Herramientas</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Noticias</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Magister</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Cotizacion</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+        <div class="custom-video">
+            <iframe  class="custom-video" src="https://www.youtube.com/embed/256e6uDKepc?si=tF3EQmwJX8pNt6gr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen  autoplay muted></iframe>
+        </div>
+    </div>
+    <div class="container">
+        <p>El Centro austral de Tecnología Genómica es un grupo de investigación al alero de la Universidad de Magallanes, que busca aportar en la comunidad científica a través de un equipo de trabajo multidisciplinario conformado por profesiones de las áreas de la salud y las ciencias de la computación. A través del uso de herramientas bioinformáticas y de diferentes técnicas aplicadas en laboratorio como la secuenciación masiva, cATG busca ser ser un aporte en la investigación científica.</p>
+        <div class="row">
+            <div class="col-md-6 p-3">
+                <div class="column">
+                    <div class="row-md-8">
+                        <div id="carouselExample" class="carousel slide">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="imagen/C01.jpg" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="imagen/C02.jpg" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="imagen/C03.jpg" class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="imagen/C04.jpg" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                                data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                                data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row-md-4">
+                        <p>A través del uso de herramientas bioinformáticas y del desarrollo de aplicaciones logramos encontrar soluciones a medida para diversos problemas biológicos, junto con esto el análisis de datos, logra ser mucho más eficiente y asertivo.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 p-3">
+                <div class="ratio ratio-16x9">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2394.5616125406395!2d-70.90309532381242!3d-53.1180633526675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb2644925a2fa3f%3A0xfd1a18bf2b2881e2!2sCADI%20UMAG!5e0!3m2!1ses!2scl!4v1698333436965!5m2!1ses!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+        <div class="text-center">
+            <img src="imagen/001.jpg" alt="catg-banner" class="img-fluid">
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
 </body>
+<footer class="p-2">
+  <div class="row">
+    <div class="col-md-3">
+        <img src="imagen/logo.png" alt="catg-logo" class="footer-logo catg">
+    </div>
+    <div class="col-md-6">
+        <p class="text-center">Centro Austral de Tecnología Genómica</p>
+        <p class="text-center">Universidad de Magallanes</p>
+        <p class="text-center">Avenida Bulnes #01855, Punta Arenas, Chile</p>
+        <p class="text-center">contacto: roberto.uribe@umag.cl - marcelo.navarrete@umag.cl</p>
+        <p class="text-center">Copyright @ 2020 Centro Austral de Tecnología Genómica.</p>
+    </div>
+    <div class="col-md-3">
+        <img src="imagen/002.png" alt="catg-logo" class="footer-logo umag">
+    </div>
+  </div>
+</footer>
 </html>
+
+<style>
+    .nav-logo {
+        max-width: 100px;
+        height: auto;
+    }
+
+    .navbar-nav {
+        position: absolute;
+        left: 60%;
+    }
+
+    .custom-video {
+        width: 100%;
+        height: 500px;
+    }
+    footer {
+        font-size: 10px;
+    }
+    .footer-logo {
+        max-width: 150px;
+        height: auto; 
+        padding: auto;
+    }
+    .catg{
+        text-align: left;  
+        padding-left: 25px;
+    }
+    .umag{
+        text-align: right;  
+        padding-right: 25px;
+    }
+</style>
