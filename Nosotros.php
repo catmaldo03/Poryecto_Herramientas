506eed44696a1e2e80206d5e97fbc4256f3bc4c8
catmaldo03 <138506870+catmaldo03@users.noreply.github.com>
2023-11-06 15:23:08 -0300

    Update Nosotros.php

--- Nosotros.php
+++ Nosotros.php
@@ -7,6 +7,305 @@
     <title>Document</title>
 </head>
 <body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg bg-light px-3 py-1">
+                    <div class="container-fluid">
+                        <div>
+                            <img src="imagen/logo.png" alt="catg-logo" class="nav-logo">
+                        </div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Nosotros</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Herramientas</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Noticias</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Magister</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Cotizacion</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <div class="container">       
+        <div class="text-center">
+            <img src="imagen/001.jpg" alt="catg-banner" class="img-fluid">
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p class="text-center">Mision</p>
+                <p class="text-center">Como centro de investigación tenemos la misión de fomentar el desarrollo regional en áreas de bioinformática, a través de un grupo de investigación interdiciplinario formado por profesionales y estudiantes de doctorado y magister.</p>
+            </div>
+            <div class="col-md-4">
+                <p class="text-center">Vision</p>
+                <p class="text-center">Nuestra visión como centro de investigación es ser un referente tanto a nivel regional como nivel nacional en el campo de la investigación y desarrollo de aplicaciones bioinformáticas.</p>
+            </div>
+            <div class="col-md-4">
+                <p class="text-center">Lineas de investigacion</p>
+                <p class="text-center">Como grupo de investigación contamos con tres principales áreas de interés bioinformática:
+                    <li>Métodos de comparación de sellos mutacionales en datos obtenidos por NGS.</li>
+                    <li>Predicción de interacción antígeno-anticuerpo mediante aprendizaje automático.</li>
+                    <li>Diseño de algoritmos para ensamblaje de novo.</li>
+                </p>
+            </div>
+        </div>    
+        <div class="row">
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Marcelo Navarrete Signorile </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="630e021100060f0c4d0d021502111106170623160e02044d000f">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="2fobe">Doctor en Medicina, Universidad de Freiburg<br/>Profesor Asociado, UMAG</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+              <p class="text-center">Equipo de trabajo</p>
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Roberto Uribe-Paredes </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="f0829f929582849fde8582999295b0859d9197de939c">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="92enk">Doctor en Ciencias de la Computación, Universidad de Castilla-La Mancha<br/>Magíster en Ciencias, mención Computación, Universidad de Chile<br/>Profesor Asociado, UMAG</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Jorge González Puelma </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="a7cdc8d5c0c289c0c8c9ddc6cbc2dde7d2cac6c089c4cb">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="b9q55">Tecnólogo Médico.<br/>Estudiante de Doctorado en Ciencias, mención Biología Molecular, UMAG</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Diego Alvarez Saravia </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="16727f73717938777a607764736c56637b777138757a">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="vrlzm">Ingeniero Civil en Computación e Informática.<br/>Estudiante de Magister en Bioinformática, UMAG.</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Julieta Sepulveda Yañez </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="f69c839a9f938297d8859386839a80939297b6839b9791d8959a">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="ayl5h">Tecnóloga Médico.<br/>Estudiante de Doctorado, Leiden University Medical Center</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Jorge Torres Almonacid </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="1b7174697c7e356f7469697e685b6e767a7c357877">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="mfrf4">Ingeniero Civil en Computación e Informática.<br/>Estudiante de Magister en Bioinformática, UMAG.</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Hermy Alvarez Ojeda </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="a3cbc6d1ceda8dc2cfd5c2d1c6d9e3d6cec2c48dc0cf">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="ribl8">Enfermera.<br/>Estudiante de Doctorado en Ciencias, mención en Biología Molecular, UMAG</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Jacqueline Aldridge Águila </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="f39992908286969f9a9d96dd929f97819a979496b3869e9294dd909f">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="3mypf">Ingeniera Civil en Computación e Informática.<br/>Estudiante de Magister en Bioinformática, UMAG.</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Ines Cid Fariña </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="afc6c1cadc81ccc6cbefdac2cec881ccc3">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="lwnrn">Licenciada en Ciencias Biológicas, Licenciada en Educación y, Profesora de Biología y Ciencias de la Naturaleza<br/>Estudiante de Doctorado en Ciencias, mención Biología Molecular</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Jose Fernandez Goicolea </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="e78d889482c981829589868983829da7928a8680c9848b">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="6r95p">Ingeniero Matemático.<br/>Estudiante de Doctorado, Universidad de Santiago de Chile</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Constanza Ceroni Momberg </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="33505056415c5d5a73465e52541d505f">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="pp2mi">Bióloga Marina de la Universidad Austral de Chile<br/>Estudiante de Magister en Ciencias con mención en Manejo y Conservación de Recursos subantárticos, UMAG</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Julio Águila Guerrero </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="afc5dac3c6c081cec8dac6c3ceefdac2cec881ccc3">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="6bpys">Ingeniero Ejecución en Computación e Informática.<br/>Doctor en Tecnologías Informáticas Avanzadas, Universidad de Castilla-La Mancha</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Carolina Perez Troncoso </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="3f5c5e4d505356515e5e51585a53565c5e114f5a4d5a457f4a525e58115c53">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="btrp4">Bióloga Marina, Universidad de Concepción.<br/>Estudiante de Magister en ciencias con mención en Manejo y conservación de recursos Naturales en ambientes Subantarticos, UMAG.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">David Medina Ortiz </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="1c787d6a75783271797875727d5c69717d7b327f70">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="3m85s">Ingeniero en Bioinformática, Universidad de Talca.<br/>Doctor en ciencias de la Ingeniería, Universidad de Chile</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Sebastián Zambrano Rojas </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="ed9e888f978c808f9fad98808c8ac38e81">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="zolic">Biólogo de la Universidad de Concepción.<br/>Estudiante de Magister en Bioinformática, UMAG.</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <img src="..." class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-2">
+                <h5 class="mb-0">Lindybeth Sarmiento Varon </h5>
+                <p class="text-center text-break p-1"> <a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="14787d7a706d7671607c3a677566797d717a607b54617975733a7778">[email&#160;protected]</a></p>
+                <div class="text-left">
+                    <p data-block-key="380lo"></p><p data-block-key="dvmne">Biólogo.<br/>Investigador asistente.<br/>Estudiante doctorado Medicina Básica. Mención Inmunología<br/>Universidad de Buenos Aires -Argentina</p>
+                </div>
+            </div>
+            <div class="col-md-2">
+            </div>
+        </div>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
+
+<footer class="p-2">
+  <div class="row">
+    <div class="col-md-3">
+        <img src="imagen/logo.png" alt="catg-logo" class="footer-logo catg">
+    </div>
+    <div class="col-md-6">
+        <p class="text-center">Centro Austral de Tecnología Genómica</p>
+        <p class="text-center">Universidad de Magallanes</p>
+        <p class="text-center">Avenida Bulnes #01855, Punta Arenas, Chile</p>
+        <p class="text-center">contacto: roberto.uribe@umag.cl - marcelo.navarrete@umag.cl</p>
+        <p class="text-center">Copyright @ 2020 Centro Austral de Tecnología Genómica.</p>
+    </div>
+    <div class="col-md-3">
+        <img src="imagen/002.png" alt="catg-logo" class="footer-logo umag">
+    </div>
+  </div>
+</footer>
 </html>
+<style>
+    .nav-logo {
+        max-width: 100px;
+        height: auto;
+    }
+
+    .navbar-nav {
+        position: absolute;
+        left: 60%;
+    }
+    footer {
+        font-size: 10px;
+    }
+    .footer-logo {
+        max-width: 150px;
+        height: auto; 
+        padding: auto;
+    }
+    .catg{
+        text-align: left;  
+        padding-left: 25px;
+    }
+    .umag{
+        text-align: right;  
+        padding-right: 25px;
+    }
+</style>
