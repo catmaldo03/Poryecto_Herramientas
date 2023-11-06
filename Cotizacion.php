b495d95511408fb9e3cd4e83f1b876376bb354a4
catmaldo03 <138506870+catmaldo03@users.noreply.github.com>
2023-11-06 15:19:07 -0300

    Update Cotizacion.php

--- Cotizacion.php
+++ Cotizacion.php
@@ -7,6 +7,82 @@
     <title>Document</title>
 </head>
 <body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>   
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg bg-light px-3 py-1">
+                    <div class="container-fluid">
+                        <div>
+                            <img src="imagen/logo.png" alt="catg-logo" class="nav-logo">
+                        </div>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                            <div class="navbar-nav">
+                                <ul class="navbar-nav ml-auto">
+                                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Nosotros</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Herramientas</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Noticias</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Magister</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="#">Cotizacion</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe5I0UUDyDGoi0ZbS-21RHtbBtoR_JRM3XAHl4eajvAz0ILAg/viewform?embedded=true" width="640" height="1089" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
+
+<footer class="p-2">
+  <div class="row">
+    <div class="col-md-3">
+        <img src="imagen/logo.png" alt="catg-logo" class="footer-logo catg">
+    </div>
+    <div class="col-md-6">
+        <p class="text-center">Centro Austral de Tecnología Genómica</p>
+        <p class="text-center">Universidad de Magallanes</p>
+        <p class="text-center">Avenida Bulnes #01855, Punta Arenas, Chile</p>
+        <p class="text-center">contacto: roberto.uribe@umag.cl - marcelo.navarrete@umag.cl</p>
+        <p class="text-center">Copyright @ 2020 Centro Austral de Tecnología Genómica.</p>
+    </div>
+    <div class="col-md-3">
+        <img src="imagen/002.png" alt="catg-logo" class="footer-logo umag">
+    </div>
+  </div>
+</footer>
 </html>
+<style>
+    .nav-logo {
+        max-width: 100px;
+        height: auto;
+    }
+
+    .navbar-nav {
+        position: absolute;
+        left: 60%;
+    }
+    footer {
+        font-size: 10px;
+    }
+    .footer-logo {
+        max-width: 150px;
+        height: auto; 
+        padding: auto;
+    }
+    .catg{
+        text-align: left;  
+        padding-left: 25px;
+    }
+    .umag{
+        text-align: right;  
+        padding-right: 25px;
+    }
+</style>
