c4a54f152e4ad23b19d9a868fbfd1903e408f0bf
catmaldo03 <138506870+catmaldo03@users.noreply.github.com>
2023-11-07 10:00:35 -0300

    Update_2 Index.php

--- Index.php
+++ Index.php
@@ -12,7 +12,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                <nav class="navbar navbar-expand-lg bg-light px-3 py-1">
+                <nav class="navbar navbar-expand-lg px-3 py-1">
                     <div class="container-fluid">
                         <div>
                             <img src="imagen/logo.png" alt="catg-logo" class="nav-logo">
@@ -26,11 +26,11 @@
                             <div class="navbar-nav">
                                 <ul class="navbar-nav ml-auto">
                                     <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Inicio</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="#">Nosotros</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="#">Herramientas</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="#">Noticias</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="#">Magister</a></li>
-                                    <li class="nav-item"><a class="nav-link" href="#">Cotizacion</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="Nosotros.php">Nosotros</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="Herramientas.php">Herramientas</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="Noticias.php">Noticias</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="Magister.php">Magister</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="Cotizacion.php">Cotizacion</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -44,11 +44,11 @@
         </div>
     </div>
     <div class="container">
-        <p>El Centro austral de Tecnología Genómica es un grupo de investigación al alero de la Universidad de Magallanes, que busca aportar en la comunidad científica a través de un equipo de trabajo multidisciplinario conformado por profesiones de las áreas de la salud y las ciencias de la computación. A través del uso de herramientas bioinformáticas y de diferentes técnicas aplicadas en laboratorio como la secuenciación masiva, cATG busca ser ser un aporte en la investigación científica.</p>
-        <div class="row">
+        <p class="custom-c">El Centro austral de Tecnología Genómica es un grupo de investigación al alero de la Universidad de Magallanes, que busca aportar en la comunidad científica a través de un equipo de trabajo multidisciplinario conformado por profesiones de las áreas de la salud y las ciencias de la computación. A través del uso de herramientas bioinformáticas y de diferentes técnicas aplicadas en laboratorio como la secuenciación masiva, cATG busca ser ser un aporte en la investigación científica.</p>
+        <div class="row ">
             <div class="col-md-6 p-3">
                 <div class="column">
-                    <div class="row-md-8">
+                    <div class="row-md-8 custom-b">
                         <div id="carouselExample" class="carousel slide">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
@@ -77,13 +77,13 @@
                         </div>
                     </div>
                     <div class="row-md-4">
-                        <p>A través del uso de herramientas bioinformáticas y del desarrollo de aplicaciones logramos encontrar soluciones a medida para diversos problemas biológicos, junto con esto el análisis de datos, logra ser mucho más eficiente y asertivo.</p>
+                        <p class="custom-a">A través del uso de herramientas bioinformáticas y del desarrollo de aplicaciones logramos encontrar soluciones a medida para diversos problemas biológicos, junto con esto el análisis de datos, logra ser mucho más eficiente y asertivo.</p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 p-3">
-                <div class="ratio ratio-16x9">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2394.5616125406395!2d-70.90309532381242!3d-53.1180633526675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb2644925a2fa3f%3A0xfd1a18bf2b2881e2!2sCADI%20UMAG!5e0!3m2!1ses!2scl!4v1698333436965!5m2!1ses!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="ratio ratio-16x9 custom-map">
+                <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2394.5616125406395!2d-70.90309532381242!3d-53.1180633526675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbdb2644925a2fa3f%3A0xfd1a18bf2b2881e2!2sCADI%20UMAG!5e0!3m2!1ses!2scl!4v1698333436965!5m2!1ses!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
@@ -124,6 +124,10 @@
     .navbar-nav {
         position: absolute;
         left: 60%;
+        
+    }
+    .navbar{
+        background:#2C3E7E;  
     }
 
     .custom-video {
@@ -131,7 +135,10 @@
         height: 500px;
     }
     footer {
-        font-size: 10px;
+        font-size: 15px;
+        color:white;
+        background:#2C3E7E;
+        padding: 10px;
     }
     .footer-logo {
         max-width: 150px;
@@ -146,4 +153,20 @@
         text-align: right;  
         padding-right: 25px;
     }
+    .custom-a{
+        background-color:  #2C3E7E;
+        padding: 25px;
+        color: white;
+    }
+    .custom-b{
+        border: 10px solid #2C3E7E;
+    }
+    .custom-map{
+        border: 10px solid #2C3E7E;
+        padding: 130px;
+    }
+    .custom-c{
+        font-size: 20px;
+    }
+
 </style>
